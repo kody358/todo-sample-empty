fbd1b9982be5873edbecaa8fad0735d774f052a4
kody <kody.dev358@gmail.com>
2025-03-30 09:55:56 +0900

    フィードバックを入力・表示するコンポーネントを作成

--- src/pages/practice/01.tsx
+++ src/pages/practice/01.tsx
@@ -1,15 +1,30 @@
 import { NextPage } from 'next';
 
 import Button from '@/components/common/parts/Button';
-import { COLORS, useChangeBgColor } from '@/hooks/useChangeBgColor';
+import { useDisplayFeedBack } from '@/hooks/useDisplayFeedBack';
 
 const Page: NextPage = () => {
-  const { currentColorIndex, changeColor } = useChangeBgColor();
+  const { feedbackList, handleInputChange, handleSubmit, inputValue } = useDisplayFeedBack();
 
   return (
-    <div className="h-screen pt-8" style={{ backgroundColor: COLORS[currentColorIndex] }}>
+    <div className="mx-auto mt-8 max-w-4xl">
       <div className="flex justify-center">
-        <Button onClick={changeColor} label="色を変更" variant="primary" />
+        <div>
+          <textarea
+            className="rounded-sm border px-3 py-2"
+            placeholder="フィードバックを入力してください"
+            value={inputValue}
+            onChange={handleInputChange}
+          ></textarea>
+          <div className="mt-4 flex justify-center">
+            <Button onClick={handleSubmit} label="送信する" variant="primary" />
+          </div>
+          <div className="mt-2">
+            {feedbackList.map((feedback, index) => (
+              <li key={index}>{feedback}</li>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
