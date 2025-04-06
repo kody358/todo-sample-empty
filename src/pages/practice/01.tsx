d5ec47a1ddbd3682a6c3ab9b6775984d8c8c987c
kody <kody.dev358@gmail.com>
2025-04-06 09:59:48 +0900

    ランダムな名言表示コンポーネントを作成

--- src/pages/practice/01.tsx
+++ src/pages/practice/01.tsx
@@ -1,15 +1,15 @@
 import { NextPage } from 'next';
 
-import { useDigitalClock } from '@/hooks/useDigitalClock';
+import { useQuotesRandom } from '@/hooks/useQuotesRandom';
 
 const Page: NextPage = () => {
-  const { currentTime } = useDigitalClock();
+  const { currentQuote } = useQuotesRandom();
 
   return (
     <div className="mx-auto mt-8 max-w-4xl">
       <div className="flex justify-center">
         <div>
-          <h3 className="text-center text-2xl">{currentTime.toLocaleTimeString()}</h3>
+          <h3 className="text-center text-2xl">{currentQuote}</h3>
         </div>
       </div>
     </div>
